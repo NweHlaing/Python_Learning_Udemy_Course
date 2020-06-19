340c58ad9b351e01a47868f247514fc8f5efd1f2
Nwe Nwe Hlaing <34648091+NweHlaing@users.noreply.github.com>
2020-06-19 15:51:48 +0630

    feat: Pangram
    Assignment No7

---
+++ Section_6_Methods_and_Functions/Ass7_Pangram.py
@@ -0,0 +1,7 @@
+#Pangram check
+import string
+
+def ispangram(str1, alphabet=string.ascii_lowercase):  
+    alphaset = set(alphabet)  
+    return alphaset <= set(str1.lower())  
+print("isPangram.....",ispangram("hello"))
