9b916f5df30b864e7cbf147e8287986f9659ddc4
Nwe Nwe Hlaing <34648091+NweHlaing@users.noreply.github.com>
2020-06-27 13:12:37 +0630

    feat: return function

---
+++ Section_12_Python_Decorator/return_function.py
@@ -0,0 +1,16 @@
+def hello(name='Jose'):
+    
+    def greet():
+        return '\t This is inside the greet() function'
+    
+    def welcome():
+        return "\t This is inside the welcome() function"
+    
+    if name == 'Jose':
+        return greet
+    else:
+        return welcome
+
+data = hello()
+print(data)
+print(data())
