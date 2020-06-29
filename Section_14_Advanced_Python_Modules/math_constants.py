c44be1230cf375690cd0280018d22179245ad514
Nwe Nwe Hlaing <34648091+NweHlaing@users.noreply.github.com>
2020-06-29 21:11:43 +0630

    feat: math_constants

---
+++ Section_14_Advanced_Python_Modules/math_constants.py
@@ -0,0 +1,20 @@
+#Mathematical Constants
+import math
+math.pi
+from math import pi
+print("pi...",pi)
+print("e...",math.e)
+print("tau...",math.tau)
+print("inf...",math.inf)
+print("nan...",math.nan)
+
+#Logrithmic value
+print("e...",math.e)
+
+# Log Base e
+print("log e...",math.log(math.e))
+print("log 10....",math.log(10))
+print("math e....",math.e ** 2.302585092994046)
+print("sine....",math.sin(10))
+print("degree....",math.degrees(pi/2))
+print("radian....",math.radians(180))
