1c183b8e74d42311b56440861fffe8b6c607d62a
Nwe Nwe Hlaing <34648091+NweHlaing@users.noreply.github.com>
2020-06-29 20:44:02 +0630

    feat: rounding_numbers

---
+++ Section_14_Advanced_Python_Modules/rounding_numbers.py
@@ -0,0 +1,7 @@
+import math
+# print("Help Math..",help(math))
+
+value = 4.35
+print("Floor Value....",math.floor(value))
+print("Celing Value...",math.ceil(value))
+print("Round Value....",round(value))
